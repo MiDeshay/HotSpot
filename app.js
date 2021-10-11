34fe91581cd30bb7c17693ef8aa6f4a51f78d6a2
tkla <kennyla95@gmail.com>
2021-10-11 09:27:45 -0700

    Add initial backend login skeleton

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const express = require("express");
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
-const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
@@ -28,7 +27,6 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/users", users);
-app.use("/api/tweets", tweets);
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
