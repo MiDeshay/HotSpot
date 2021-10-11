66c262593ee64fde987ed60d35eda7ad786ba6f4
tkla <kennyla95@gmail.com>
2021-10-11 16:13:32 -0700

    Add pins_container.jsx which on click returns their location in console

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,37 +1,48 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
+import PinsContainer from './pins_container';
 
 const mapsAPI = "AIzaSyBvn9RZTw5O6r1sV18x8gnvKztsRicJLxk";
 export default class Home extends React.Component{
+
    constructor(props){
       super(props)
+
+      this.center = {
+         lat: -34.397, 
+         lng: 150.644 
+      }
+      this.zoom = 11;
+      this.handleApiLoaded = this.handleApiLoaded.bind(this);
    }
 
    componentDidMount(){
+   }
 
+   handleApiLoaded(map, maps){
+      return;
    }
 
    render(){
-      
-      let api = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBvn9RZTw5O6r1sV18x8gnvKztsRicJLxk&q=Eiffel+Tower,Paris+France` ;
+      const Pin = ({ text }) => <div onClick={console.log(this.props)}>{text}</div>;
+
       return (
          <div style={{ height: '100vh', width: '100%' }}>
             <GoogleMapReact
                bootstrapURLKeys={{ key: mapsAPI }}
-               defaultCenter={this.props.center}
-               defaultZoom={this.props.zoom}
+               defaultCenter={this.center}
+               defaultZoom={this.zoom}
+               yesIWantToUseGoogleMapApiInternals
+               onGoogleApiLoaded={({ map, maps }) => this.handleApiLoaded(map, maps)}
             >
-               <AnyReactComponent
-                  lat={59.955413}
-                  lng={30.337844}
+               <PinsContainer
+                  lat={-34.397}
+                  lng={150.644}
                   text="My Marker"
+                  
                />
             </GoogleMapReact>
          </div>
       )  
    }
 }
-
-{/* <iframe title='yolo' width="1500" height="700" frameborder="0" 
-   src={api} allowfullscreen="">
-</iframe> */}
